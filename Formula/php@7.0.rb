981a3d4c2198cdb8f31384227c0384c4900abcc6
Patrick Gagnon-Renaud <pgrenaud@exolnet.com>
2019-01-31 16:25:44 -0500

    php@7.0: remove checks for old compilers.

--- Formula/php@7.0.rb
+++ Formula/php@7.0.rb
@@ -42,8 +42,6 @@ class PhpAT70 < Formula
   # see https://github.com/php/php-src/pull/3472
   patch :DATA
 
-  needs :cxx11
-
   def install
     # Ensure that libxml2 will be detected correctly in older MacOS
     if MacOS.version == :el_capitan || MacOS.version == :sierra
