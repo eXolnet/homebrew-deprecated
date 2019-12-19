e81a5297bb9de3036f47672c4ec0eb3529bffff3
Patrick Gagnon-Renaud <pgrenaud@exolnet.com>
2019-12-19 15:12:54 -0500

    openssl@1.0: update 1.0.2t bottle.

--- Formula/openssl@1.0.rb
+++ Formula/openssl@1.0.rb
@@ -10,10 +10,8 @@ class OpensslAT10 < Formula
   sha256 "14cb464efe7ac6b54799b34456bd69558a749a4931ecfd9cf9f71d7881cac7bc"
 
   bottle do
-    sha256 "c9c5e017edabe41ae55ed10ba5b94b834ee494e7f362d7245fbb0b137c876810" => :catalina
-    sha256 "9874b2baf00f845355b163cb63b5c98a94a5cf7c08cda1d19876899b11b585c6" => :mojave
-    sha256 "20fa4d39cbc0ba091aed2ce72a4404e87c3bc323243ab3f92ccfd75c48cbe132" => :high_sierra
-    sha256 "bdbc44c56f63f27ab4dc12583b7f46a6485500f2a583dc8c9b848c4063f58927" => :sierra
+    root_url "https://dl.bintray.com/exolnet/bottles-deprecated"
+    sha256 "260d93fbf7ab0e3dc7966d935a604f47cc61d473bdadad206326d0ddf673cb65" => :catalina
   end
 
   keg_only :provided_by_macos,
